53e95de1bfa18a29ac763192414116a3cbce9e6a
json1c <sergeyfilippov000@gmail.com>
2021-10-21 11:29:26 +0300

    Updated the process of adding accounts

--- sessions/add_session.py
+++ sessions/add_session.py
@@ -1,14 +1,17 @@
-import sys
+import os
+import string
+import random
 import toml
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-name = sys.argv[1]
 with open("../config.toml") as file:
     config = toml.load(file)["sessions"]
 
 api_id = config["api_id"]
 api_hash = config["api_hash"]
+
+name = "".join(random.sample(string.ascii_letters, 10))
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
     with open(f"{name}.session", "w") as file:
