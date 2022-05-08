c2f7c7a2d5be27856a7dedd4d84f9616332affee
json1c <48158038+json1c@users.noreply.github.com>
2022-05-08 20:41:22 +0300

    Video raid

---
+++ functions/voice_chat.py
@@ -0,0 +1,97 @@
+# https://github.com/json1c
+# Copyright (C) 2021  json1c
+
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 3 of the License
+
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
+
+import asyncio
+from pytgcalls import idle
+from pytgcalls.types import AudioPiped, VideoPiped, AudioVideoPiped, AudioImagePiped, LowQualityVideo
+from pytgcalls import PyTgCalls
+from telethon import utils
+from rich.console import Console
+from youtube_dl import YoutubeDL
+
+from functions.function import Function
+
+console = Console()
+
+
+class VoicePlayFunc(Function):
+    """Join voice chat and play audio"""
+
+    async def join_and_play(self, session):
+        await session.start()
+
+        app = PyTgCalls(session)
+        await app.start()
+        entity = await session.get_entity(self.chat)
+
+        if self.format_choice == "1":
+            await app.join_group_call(
+                utils.get_peer_id(entity),
+                AudioPiped(self.media_url),
+            )
+        
+        elif self.format_choice == "2":
+            await app.join_group_call(
+                utils.get_peer_id(entity),
+                AudioVideoPiped(self.media_url, video_parameters=LowQualityVideo())
+            )
+
+        await idle()
+
+
+    async def execute(self):
+        self.ask_accounts_count()
+
+        self.chat = console.input("[bold red]chat link> [/]")
+
+        console.print(
+            "\n[bold white][1] Audio\n"
+            "[2] Video\n"
+        )
+
+        self.format_choice = console.input("[bold white]>> [/]")
+        
+        console.print(
+            "\n[bold white][1] From Youtube\n"
+            "[2] From direct link to file[/]\n"
+        )
+
+        source_choice = console.input("[bold white]>> [/]")
+
+        if source_choice == "1":
+            url = console.input("[bold red]video url> [/]")
+            ydl = YoutubeDL()
+
+            r = ydl.extract_info(url, download=False)
+
+            if self.format_choice == "1":
+                for rformat in r["formats"]:
+                    if rformat["fps"] is None:
+                        self.media_url = rformat["url"]
+                        break
+            
+            elif self.format_choice == "2":
+                for rformat in r["formats"]:
+                    if "144" in rformat["format"]:
+                        print(rformat)
+                        self.media_url = rformat["url"]
+                        break
+        
+        elif source_choice == "2":
+            self.media_url = console.input("[bold red]media url> [/]")
+
+
+        await asyncio.wait([
+            self.join_and_play(session)
+            for session in self.sessions
+        ])
