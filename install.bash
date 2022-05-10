69ca65f439792f3f905ee4e5adafcd400ff0df28
lkqas <86144499+lkqas@users.noreply.github.com>
2022-05-10 16:59:52 +0200

    minor fix

--- install.bash
+++ install.bash
@@ -75,7 +75,7 @@ elif cat /etc/*release | grep ^NAME | grep -r Arch || cat /etc/*release | grep ^
             echo "Please launch autoinstall with root"
             exit 1;
     fi
-elif  cat /etc/*release | grep ^NAME | grep Ubuntu || cat /etc/*release | grep ^NAME | grep Debian || cat /etc/*release | grep ^NAME | grep Mint; then
+elif  cat /etc/*release | grep ^NAME | grep Ubuntu || cat /etc/*release | grep ^NAME | grep Debian || cat /etc/*release | grep ^NAME | grep Mint || cat /etc/*release | grep ^NAME | grep Mint; then
     if [[ $(whoami) = 'root' ]]; then
         clear
         echo "Detected OS : $(cat /etc/*release | grep ^NAME)"
@@ -96,7 +96,7 @@ elif  cat /etc/*release | grep ^NAME | grep Ubuntu || cat /etc/*release | grep ^
         exit 1;
     fi
 elif  [[ $(uname -o) = 'Android' ]]; then
-    if command -v termux-setup-storage; then
+    if echo $PREFIX | grep -o "com.termux"; then
         clear
         echo "Detected System : Android (termux)"
         echo "Package manager : pkg"
