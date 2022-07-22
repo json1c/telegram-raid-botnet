d589091dd142286c9b90f4ea0eb414f24eb5f4eb
json1c <48158038+json1c@users.noreply.github.com>
2022-07-22 12:20:11 +0300

    Update 2fa function

--- functions/2fa.py
+++ functions/2fa.py
@@ -12,19 +12,41 @@
 # If not, see <https://www.gnu.org/licenses/>.
 
 import asyncio
+
 from rich.console import Console
+from rich.markup import escape
+
+from telethon import TelegramClient
 from functions.base import TelethonFunction
+
 console = Console()
 
 class SetPasswordFunc(TelethonFunction):
     """Set two-step verification password to accounts"""
+    
+    async def edit_2fa(self, session: TelegramClient, password: str):
+        async with self.storage.ainitialize_session(session):
+            me = await session.get_me()
+
+            try:
+                await session.edit_2fa(new_password=password)
+            except Exception as err:
+                console.print(
+                    "[{name}] : [bold red]Password not changed[/]. Error: {error}"
+                    .format(name=escape(me.first_name), error=err)
+                )
+            else:
+                console.print(
+                    "[{name}] : [bold green]Successfully updated password"
+                    .format(name=escape(me.first_name))
+                )
 
     async def execute(self):
         password = console.input("[bold red]new password> [/]")
 
         with console.status("Setting password..."):
             await asyncio.wait([
-                session.edit_2fa(new_password=password)
+                self.edit_2fa(session=session, password=password)
                 for session in self.sessions
             ])
 
