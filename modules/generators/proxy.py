ed2c5b1adb4c0791cc947798c443c7a8828200f8
fredkek <120385937+fredkek@users.noreply.github.com>
2022-12-12 16:11:30 +0300

    Create proxy.py
    add ip list of proxy

---
+++ modules/generators/proxy.py
@@ -0,0 +1,12 @@
+
+# socks.SOCKS4 = 1
+# socks.SOCKS5 = 2
+# socks.HTTP = 3
+
+# IP[№IP][ip, port, type socks (int), login, pass]
+
+
+IP = [
+#['128.0.0.1', 1111, 2, 'login', 'pass'],
+#['128.0.0.1', 47243, 2]
+]
